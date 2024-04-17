fbd2b8d73a8c9848444b4fae2731d3093ab023a5
jang <treous11@gmail.com>
2024-04-17 13:47:55 +0900

    init

--- src/string/Quiz6.java
+++ src/string/Quiz6.java
@@ -6,15 +6,14 @@ public class Quiz6 {
 
 	public static void main(String[] args) {
 
-		System.out.println("문장을 입력하세요:");
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
 
 		//endsWith("end")
-		int i = 0;
-		while (i >= 0 ) {
-			String str2 = scanner.nextLine();
-			if(str2.endsWith("end") || str2.endsWith("END")) {
+		while (true) {
+			System.out.print("문장을 입력하세요:");
+			String str = scanner.nextLine();
+//			if(str.endsWith("end") || str.endsWith("END")) {
+			if(str.equalsIgnoreCase("end")) {
 				break;
 			}
 		} 
